b24321e155501769639c918a2f9791d4dbae53b1
tomjes03 <sonofdsonofgod@yahoo.com>
2023-06-15 19:12:21 +0100

    Reacr-chekpoint task commit message

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from './components/navbar/Nav'
+import Carousel from './components/carousel/Carousel'
+import Form from './components/form/Form'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Carousel />
+      <Form />
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
