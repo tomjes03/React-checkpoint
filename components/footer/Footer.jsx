d1d089fce4f4db5723a9fd2dc5294cfc4f095398
tomjes03 <sonofdsonofgod@yahoo.com>
2023-06-15 19:22:08 +0100

    React-checkpoint task commit message

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer>
+            <div class="footer">&copy; copyright Emmanuel Nwafor</div>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
